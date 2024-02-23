65a5c5f5d2b8d3aee4adfcbe43283a6e91482f20
Hung <vanhungnvh1712004@nvh1712004gmail.com>
2024-02-23 10:13:25 +0700

    zalo

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose')
+require('dotenv').config()
+
+const ConnectData=async()=>{
+    try {
+        
+        await mongoose.connect(process.env.URL)
+            console.log('Connect successfully')
+    } catch (error) {
+        console.log('Can not connect')
+    }
+}
+module.exports=ConnectData
